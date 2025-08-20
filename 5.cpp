dd982f45df288eca5eb711a4415600f47d162258
ChargingTrex <158301945+ChargingTrex@users.noreply.github.com>
2025-08-20 10:37:10 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main(){
+    int a,s;
+    while (true) {
+        cout << "Enter a integer: ";
+        cin >> a;
+        s+=a;
+        if(s>=100){
+            break; 
+    }
+}
+return 0;   
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
